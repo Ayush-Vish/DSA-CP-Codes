8e86208edd0a1c379571a37317e9eb6622609716
Ayush Vishwakarma <127919475+Ayush-Vish@users.noreply.github.com>
2023-07-28 11:56:49 +0530

    Time: 13 ms (81.57%) | Memory: 13.3 MB (60.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,26 +3,24 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n  = matrix.size() ;
         int m  = matrix[0].size();
-        map< pair<int ,int >  ,bool >  mpp;
+        vector<int>  row(n,0); 
+        vector<int>  col(m,0); 
+        
+        
         for(int i =0 ; i < n ; i++ ) {
             for(int j =0 ; j < m ;j++) {
                 if(matrix[i][j] == 0) {
-                    for(int k  =  0; k < matrix.size() ;k++) {
-                        if(matrix[k][j] != 0 ) mpp[make_pair(k,j)] = true;
-                    }
-                    for(int k  =  0; k < matrix[i].size() ;k++) {
-                        if(matrix[i][k] != 0 ) mpp[make_pair(i,k)] = true;
-                    }
+                    row[i] = 1 ;
+                    col[j] = 1;
                 }
                 
             }
         }
-        for(int i =0 ;i  < n ;i ++  ) {
-            for(int j =0 ;j  < m ;j++ ) {
-                if(mpp[{i,j}] == 1  ) {
-                    matrix[i][j]  = 0 ;
-                }
+        for(int i=0  ;i  < n ;i++ ) {
+            for(int j =0 ; j < m ;j++ ) {
+                if (row[i] ==1||col[j] == 1 ) matrix[i][j] = 0 ;
             }
         }
+        
     }
 };
