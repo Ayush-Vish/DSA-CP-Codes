707580f8ff508f4127a7981d3556279bf94777cc
Ayush Vishwakarma <127919475+Ayush-Vish@users.noreply.github.com>
2023-07-29 14:51:13 +0530

    Time: 92 ms (15.24%) | Memory: 72.6 MB (6.48%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     void solve(vector<int> arr, int target , vector<int> v, vector<vector<int>> &ans, int  i ) { 
         if(i>=arr.size() ) {
-            return ;
+            if(target == 0 ) {
+                ans.push_back(v);
+                return ;
+            }else {
+                return;
+            }
         }
-        if(target == 0 ) {
-            ans.push_back(v);
-            return ;
-        }
-        if(target <0) {
-            return ;
-        }
-        v.push_back(arr[i]) ;
+        if(arr[i] <= target) {
+            v.push_back(arr[i]) ;
 
-        solve(arr ,target - arr[i] , v, ans, i );
-        v.pop_back();
-        target += arr[i];
-        solve(arr , target - arr[i]  , v,ans , i +1) ;
+            solve(arr ,target - arr[i] , v, ans, i );
+            v.pop_back();
+        }
+       
+        solve(arr , target  , v,ans , i +1) ;
         
 
     }
