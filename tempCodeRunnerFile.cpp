2642a092d67912b4f20e5e44f477677f5abfb779
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2024-05-30 20:01:58 +0530

    Before cf -166

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,1 @@
-
-   int n , x ;
-   vector<int> arr(n);
-    takeInput(arr,n);
-    int dis1= 2*(x-arr[n-1]);
-    int ans = INT_MAX;
-    ans=min(ans , dis1);
-    for(int i =n-1 ;i>=1 ;i--) {
-        ans = min(arr[i] -arr[i-1] , ans ) ;
-    }
-    cout << min(arr[0],ans) <<endl;
-}
+long long int
