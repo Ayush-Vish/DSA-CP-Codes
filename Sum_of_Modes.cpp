be59cc30e6e07d5d8170fda72f4d14b61182c0bb
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2024-06-04 01:12:07 +0530

    ✨ Added new Questions
    Added Questions from CF 190

--- Sum_of_Modes.cpp
+++ Sum_of_Modes.cpp
@@ -1,61 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-bool checkSorted(vector<int> &a)
-{
-    for (int i = 0; i < a.size() - 1; i++)
-    {
-        if (a[i] > a[i + 1])
-            return false;
-    }
-    return true;
-}
-void takeInput(vector<int> &a, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        int ele;
-        cin >> ele;
-        a[i] = ele;
-    }
-}
-
-void printArr(vector<int> &a)
-{
-    cout << endl;
-    for (auto it : a)
-    {
-        cout << it << " ";
-    }
-    cout << endl;
-}
+#define ll long long
+#define pb push_back
+#define pll pair<ll, ll>
+bool checkSorted(vector<int> &a) { for (int i = 0; i < a.size() - 1; i++) { if (a[i] > a[i + 1]) return false; } return true; } template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } } template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
 void solve()
 {
-   int n;
-   cin >> n;
-   string s;
-   cin >> s;
-   int one=0;
-    int zero=0;
-    int ans =0 ;
-
-   for(int i=0;i<n;i ++ ) {
-      for(int j=i;j<n;j++) {
-        if(s[j]=='1') {
-            one++;
+    
+    ll n ;
+    cin >> n;
+    string s;
+    cin >> s; 
+    unordered_map<ll,ll> mpp;
+    ll ans =0 ;
+    ll diff  =0 ;
+    for(int i=0;i< n;i++ ) {
+        if(s[i] == '0' ) {
+            diff++;
         } else {
-            zero++;
+            diff--;
         }
-        if(one== zero) {
-            ans+=2;
-        }else {
-            ans ++ ;
+
+        if(diff == 0) {
+            ans++;
         }
-      }
-      one  =0; 
-      zero =0;     
-   }
-   cout <<ans << endl;
+        ans  += mpp[diff];
+        mpp[diff] ++ ;
+    }
+
+
+    cout <<( (n*(n+1))/2) + ans << endl;
 }
 int main()
 {
