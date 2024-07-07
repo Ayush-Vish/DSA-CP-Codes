05d0ce2ddf3d8793e5b724f7a42df4b30685cd12
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2024-07-07 20:04:35 +0530

    🔥 Before CF -956

--- A_Halloumi_Boxes.cpp
+++ A_Halloumi_Boxes.cpp
@@ -1,44 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-bool checkSorted(vector<int> &a)
-{
-    for (int i = 0; i < a.size() - 1; i++)
-    {
-        if (a[i] > a[i + 1])
-            return false;
-    }
-    return true;
-}
-void takeInput(vector<int> &a, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        int ele;
-        cin >> ele;
-        a[i] = ele;
-    }
-}
-void printArr(vector<int> &a)
-{
-    cout << endl;
-    for (auto it : a)
-    {
-        cout << it << " ";
-    }
-    cout << endl;
-}
-void solve()
+#define int long long int
+class Node
 {   
-   
-}
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
     {
-        solve();
+        this->left = nullptr;
+        this->right = nullptr;
+        this->data = data;
     }
+};
+Node *buildTree(Node *root)
+{
+    cout << "Enter the data" << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+    cout << "Enter data for inserting in the left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in the right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+void levelOrderTraversal(Node *root){
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL){
+            // previous level is commmpletely 
+            // Next Line 
+            cout << endl;
+            if (!q.empty()){
+                // queue still have Some Child Nodes
+                q.push(NULL);
+            }
+        }
+        else{
+            cout << temp->data << " ";
+            if (temp->left){
+                q.push(temp->left);
+            }
+            if (temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int32_t main()
+{
+    Node *root = NULL;
+    root = buildTree(root);
+    levelOrderTraversal(root);
+    
     return 0;
-} 
+}
