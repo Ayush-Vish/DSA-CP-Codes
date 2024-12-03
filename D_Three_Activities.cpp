1db62cb9cbfede48386ec453d8d706a03d8c8f11
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2024-12-03 20:46:56 +0530

    🎨 Commit

--- D_Three_Activities.cpp
+++ D_Three_Activities.cpp
@@ -8,19 +8,16 @@ const int mod = 1e9 + 7;
 const int INF = 1e18;
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
-int prime[1];
-bool seive(int n){memset(prime, 1, n+1);for(int i=2;i*i<n;i++)if(prime[i]==1)for(int j=i*i;j<=n;j+=i)prime[j]=0;return true;}
-bool checkSorted(vector<int> &a) { for (int i = 0; i < a.size() - 1; i++) { if (a[i] > a[i + 1]) return false; } return true; }
 template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } }
 template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
-/**
-* Author: god_ayushv
-*/
 void solve()
 {
-    // Write your greatness here
-    
-
+    int n;
+    cin >> n;
+    vector<int>a (n) ;
+    for( int i=0 ;i < n ;i ++ ) {
+        cin >> a[i  ] ;
+    }
 }
 int32_t main()
 {
--- cses/Number_Spiral.cpp
+++ cses/Number_Spiral.cpp
@@ -10,20 +10,18 @@ int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } }
 template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
-void solve()
-{
-    int n;
-    cin >> n;
+void solve(){
+    int x , y ;
+    cin >> x >> y ;
+
 }
-int32_t main()
-{
+int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--){
         solve();
     }
     return 0;
