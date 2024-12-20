a1a9ef2453899b84538ea5e894fd26ca3a0a83ab
Ayush Vishwakarma <ayushvish6555@gmail.com>
2024-12-20 11:33:25 +0530

    Commit
    Signed-off-by: Ayush Vishwakarma <ayushvish6555@gmail.com>

--- cses/Introductory_problems/Bit_Strings.cpp
+++ cses/Introductory_problems/Bit_Strings.cpp
@@ -8,21 +8,32 @@ const int mod = 1e9 + 7;
 const int INF = 1e18;
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
+
+int fastExpo(int base, int exp) {
+    int res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return res;
+}
 template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } }
 template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
 void solve(){
-    int x , y ;
-    cin >> x >> y ;
+    int n;
+    cin >> n;
+    cout << fastExpo(2ll, n )<< endl;
 
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--){
+    
         solve();
-    }
+    
     return 0;
 }
