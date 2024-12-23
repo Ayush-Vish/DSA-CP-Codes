04254ef38d5924d65302ebde41d2d55cd15df86b
Ayush Vishwakarma <ayushvish6555@gmail.com>
2024-12-23 09:27:27 +0530

    Commit -a -b -s -m

--- cses/Introductory_problems/Chessboard_and_Queens.cpp
+++ cses/Introductory_problems/Chessboard_and_Queens.cpp
@@ -1,29 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long int
-#define pb push_back
-#define pll pair<int, int>
-const int modll = 1e17;
-const int mod = 1e9 + 7;
-const int INF = 1e18;
-int dx[] = {1, 0, -1, 0};
-int dy[] = {0, 1, 0, -1};
-template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } }
-template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
-int fastPow(int a, int b) { int res = 1; while (b) { if (b & 1) { res = (res * a) % mod; } a = (a * a) % mod; b >>= 1; } return res; }
-void solve(){
-    int n;
-    cin >> n;
-      
+
+bool check(int row, int col, vector<vector<char>> &v) {
+    for (int i = 0; i < row; i++) {
+        if (v[i][col] == 'Q') return false;
+    }
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (v[i][j] == 'Q') return false;
+    }
+    for (int i = row, j = col; i >= 0 && j < v.size(); i--, j++) {
+        if (v[i][j] == 'Q') return false;
+    }
+    return true;
 }
-int32_t main(){
+
+void nQueens(int row, vector<vector<char>> &v, int &ans) {
+    if (row == v.size()) {
+        ans++;
+        return;
+    }
+    for (int i = 0; i < v.size(); i++) {
+        if (check(row, i, v) && v[row][i] != '*') {
+            v[row][i] = 'Q';
+            nQueens(row + 1, v, ans);
+            v[row][i] = '.';
+        }
+    }
+}
+
+void solve() {
+    int n = 8;
+    vector<vector<char>> v(n, vector<char>(n, '.'));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> v[i][j];
+        }
+    }
+    int ans = 0;
+    nQueens(0, v, ans);
+    cout << ans << endl;
+}
+
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--){
-        solve();
-    }
+    solve();
     return 0;
 }
