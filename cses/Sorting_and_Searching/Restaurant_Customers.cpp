ebf93517a8f8c4107a62a6b78dba8b8955ec3618
Ayush Vishwakarma <ayushvish6555@gmail.com>
2025-02-05 22:12:36 +0530

    Add some Questions

--- cses/Sorting_and_Searching/Restaurant_Customers.cpp
+++ cses/Sorting_and_Searching/Restaurant_Customers.cpp
@@ -11,39 +11,31 @@ int dy[] = {0, 1, 0, -1};
 template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } }
 template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
 int fastPow(int a, int b) { int res = 1; while (b) { if (b & 1) { res = (res * a) % mod; } a = (a * a) % mod; b >>= 1; } return res; }
-
-
- 
 void solve(){
-    int n ;
-    cin >> n ;
-    vector<pair<int,pair<int,int>>> v;
-    for(int i=0 ;i < n ; i ++ ) {
-        int l,r,p;
-        cin >> l >> r >> p ;
-
-        v.push_back({r,{l,p}});
-    }
-    sort(v.begin(),v.end());
-    vector<int> dp(n+1 ,0);
-    dp[0]= 0 ;
-
-    for(int i=1 ; i <= n ; i ++ ) {
-        int start = v[i-1].second.first;
-        int reward = v[i-1].second.second;
-        int prev = lower_bound(v.begin(),v.end() , make_pair(start , make_pair(0LL,0LL))) - v.begin();
-        cout << "for i => " << i << " prev => " <<  prev << endl ; 
-        dp[i] =max(dp[i-1]  ,reward + dp[prev]);
-
-    }
-    cout << dp[n] << endl;
-
+      int n ;
+      cin >> n ;
+      map<int,int> mpp ;
+      for(int i=0 ;i < n ;i ++ ) {
+            int ele1,ele2; 
+            cin >> ele1 >> ele2  ;
+            mpp[ele1 ] =1 ;
+            mpp[ele2] = -1 ;
+      }
+      int curr =0,ans =0;
+      for(auto it  : mpp ) {
+            curr += it.second;
+            ans = max(ans, curr ) ;
+      } 
+      cout <<ans <<endl;
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-   
+    int t =1 ;
+//     cin >> t;
+    while (t--){
         solve();
+    }
     return 0;
 }
--- cses/Sorting_and_Searching/Stick_Lengths.cpp
+++ cses/Sorting_and_Searching/Stick_Lengths.cpp
@@ -11,39 +11,36 @@ int dy[] = {0, 1, 0, -1};
 template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < n; i++) { T ele; cin >> ele; a[i] = ele; } }
 template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
 int fastPow(int a, int b) { int res = 1; while (b) { if (b & 1) { res = (res * a) % mod; } a = (a * a) % mod; b >>= 1; } return res; }
-
-
- 
 void solve(){
-    int n ;
-    cin >> n ;
-    vector<pair<int,pair<int,int>>> v;
-    for(int i=0 ;i < n ; i ++ ) {
-        int l,r,p;
-        cin >> l >> r >> p ;
+      int n ;
+      cin >> n ;
+      vector<int>a(n);
+      int sum = 0 ;
 
-        v.push_back({r,{l,p}});
-    }
-    sort(v.begin(),v.end());
-    vector<int> dp(n+1 ,0);
-    dp[0]= 0 ;
+      for(int i=0 ;i < n ;i ++ ) {
+            cin >> a[i];
+            sum += a[i];
+      }
+      int ag = (sum /n) , ag2 = ((sum + n-1) /n) ;
+      cout << "Average : " << ag << " => " << ag2  << endl;
+      int ans =0,ans2 =0 ;
+      for(int i= 0 ; i <n ; i++) {
+            ans += abs(ag - a[i]) ;
+            ans2 += abs(ag2 - a[i]) ;
+      }
 
-    for(int i=1 ; i <= n ; i ++ ) {
-        int start = v[i-1].second.first;
-        int reward = v[i-1].second.second;
-        int prev = lower_bound(v.begin(),v.end() , make_pair(start , make_pair(0LL,0LL))) - v.begin();
-        cout << "for i => " << i << " prev => " <<  prev << endl ; 
-        dp[i] =max(dp[i-1]  ,reward + dp[prev]);
+      cout << min(ans,ans2) ;
 
-    }
-    cout << dp[n] << endl;
 
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-   
+    int t =1 ;
+//     cin >> t;
+    while (t--){
         solve();
+    }
     return 0;
 }
