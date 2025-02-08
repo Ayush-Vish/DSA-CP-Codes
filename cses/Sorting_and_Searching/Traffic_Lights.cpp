17f1f3b622ebe9d8f2802d4f7705670f5955c2e3
Ayush Vishwakarma <ayushvish6555@gmail.com>
2025-02-08 23:17:01 +0530

    Added Some Cses Question

--- cses/Sorting_and_Searching/Traffic_Lights.cpp
+++ cses/Sorting_and_Searching/Traffic_Lights.cpp
@@ -12,35 +12,13 @@ template <typename T> void takeInput(vector<T> &a, int n) { for (int i = 0; i < 
 template <typename T> void printArr(vector<T> &a) { cout << endl; for (auto it : a) { cout << it << " "; } cout << endl; }
 int fastPow(int a, int b) { int res = 1; while (b) { if (b & 1) { res = (res * a) % mod; } a = (a * a) % mod; b >>= 1; } return res; }
 void solve(){
-      int n ;
-      cin  >> n  ;
-      
-      vector<int>v ;
-      int ans =0 ;
-
-      for(int i= 0 ;i < n ; i ++ ) {
-            int val ;
-            cin >> val ;
-            if(upper_bound(v.begin() , v.end() ,val ) == v.end()) {
-                  v.push_back(val);
-                //   cout << "Inserted val -> " << val << endl;
- 
-            }else {
-                // cout  << "Upper Bound => " <<  upper_bound(v.begin() , v.end() ,val ) - v.begin() << endl;
-
-                v[upper_bound(v.begin() , v.end() ,val )  - v.begin()] = val ;
-
-            }
-      }
-      cout  << v.size() << endl;
-
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int t =1 ;
-//     ci/n >> t;
+    cin >> t;
     while (t--){
         solve();
     }
