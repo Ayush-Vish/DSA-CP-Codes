f0a0efa2a378def9a20db85c62c04f75d97d3aa0
Ayush <ayushvish6555@gmail.com>
2025-07-19 10:21:05 +0530

    Added more questions

--- specialist/D_Token_Removing.cpp
+++ specialist/D_Token_Removing.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 #define int long long int
 void solve(){
-    int n,x,m ;
-    cin >> n >> x >> m ;
-    
-    int l = x , r = x ;
-    while(m --) {
-        int x, y ;
-        cin >> x >> y ;
-        
-        if(max(l,x) <= min(r,y)) {
-            l = min(x, l ) ;
-            r = max(y,r);
-        }
-    }
-    cout << r - l +1 << endl;
-
 
     
 }
